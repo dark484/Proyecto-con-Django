68340a7106540aaec707af7ffe0d1077a6676539
dark484 <ray_alexis11@hotmail.com>
2013-08-19 10:09:50 -0500

    cambios

--- aplicacionWeb/aplicacionWeb/urls.py
+++ aplicacionWeb/aplicacionWeb/urls.py
@@ -5,13 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'aplicacionWeb.views.home', name='home'),
-    # url(r'^aplicacionWeb/', include('aplicacionWeb.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^polls/', include('polls.urls')),
+	url(r'^polls/', include('polls.urls', namespace='polls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
